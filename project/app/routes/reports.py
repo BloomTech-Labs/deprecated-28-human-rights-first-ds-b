84d98ecc0d962a446841cdbdb559b52368161336
BrokenShell <webmaster@sharpdesigndigital.com>
2020-11-02 16:11:04 -0800

    adds filtering routes

---
+++ project/app/routes/reports.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from app.utilities import get_data
+
+
+router = APIRouter()
+DF = get_data()  # Global Data Source - pd.DataFrame
+
+
+@router.get('/report-by-id/')
+async def report_by_id(idx: str):
+    """ Get report by id """
+    result = DF.iloc[int(idx)]
+    return result.to_json(orient="records")
+
+
+@router.get('/report-by-city/')
+async def report_by_city(city: str):
+    """ Get report by city """
+    result = DF[DF['city'] == city]
+    return result.to_json(orient="records")
+
+
+@router.get('/report-by-state/')
+async def report_by_state(state: str):
+    """ Get report by state """
+    result = DF[DF['state'] == state]
+    return result.to_json(orient="records")
---
+++ project/app/utilities.py
@@ -0,0 +1,11 @@
+from ast import literal_eval
+
+import pandas as pd
+
+
+def get_data():
+    """ Sets up the data for api filtering """
+    df = pd.read_csv("data.csv")
+    df["src"] = df["src"].apply(literal_eval)
+    df["tags"] = df["tags"].apply(literal_eval)
+    return df
