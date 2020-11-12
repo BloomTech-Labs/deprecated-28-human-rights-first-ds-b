51aecea91ba08d7a502612a73241fac012df9a8a
BrokenShell <webmaster@sharpdesigndigital.com>
2020-11-12 08:50:17 -0800

    refactors code, avoids temp csv

--- project/app/load.py
+++ project/app/load.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import json
-import re
-import requests
 import urllib3
 from bs4 import BeautifulSoup
-import datetime
 
 
-def load_wrangle_data():
+def load_data() -> pd.DataFrame:
     """retrieves data from api, converts to dataframe
-    and saves as a csv. Adding features and rearranging 
+    Adding features and rearranging
     dataset to match previous data.csv format"""
 
-    url="https://api.846policebrutality.com/api/incidents?include=evidence"
+    url = "https://api.846policebrutality.com/api/incidents?include=evidence"
 
     http = urllib3.PoolManager()
     response = http.request('GET', url)
@@ -23,53 +20,60 @@ def load_wrangle_data():
 
     df_846 = pd.DataFrame(incidents)
 
-    #convert date to datetime
-    df_846['date'] = pd.to_datetime(df_846['date'], infer_datetime_format=True)
+    # convert date to datetime
+    df_846['date'] = pd.to_datetime(
+        df_846['date'],
+        infer_datetime_format=True,
+    )
 
-    #create converted date_text column
-    df_846['date_text'] = df_846['date'].apply(lambda x: x.strftime("%B %m"))
+    # create converted date_text column
+    df_846['date_text'] = df_846['date'].apply(lambda x: x.strftime("%B %m, %Y"))
 
-    #Create latitude feature
+    # Create latitude feature
     latitude = []
 
     for idx, row in df_846['geocoding'].iteritems():
-        l=row.keys()
-        
-        if 'lat' in l:
+        keys = row.keys()
+
+        if 'lat' in keys:
             latitude.append(row['lat'])
         else:
             latitude.append(None)
 
-    df_846['lat']=latitude
+    df_846['lat'] = latitude
 
-    #Create longitude feature
+    # Create longitude feature
     longitude = []
 
     for idx, row in df_846['geocoding'].iteritems():
-        l=row.keys()
+        keys = row.keys()
 
-        if 'long' in l:
+        if 'long' in keys:
             longitude.append(row['long'])
         else:
             longitude.append(None)
 
-    df_846['long']=longitude
+    df_846['long'] = longitude
 
     # Drop empty columns, description is also empty but needed for current format
     # Evidence also isn't needed, as well as id (pub_id needs to be renamed to id)
     df_846 = df_846.drop(columns=['data', 'evidence'])
 
-    #rename columns
-    df_846 = df_846.rename(columns={"geocoding":"geolocation", "links":"src", "pub_id":"id",
-                       "description":"desc"})
+    # rename columns
+    df_846 = df_846.rename(columns={
+        "geocoding": "geolocation",
+        "links": "src",
+        "pub_id": "id",
+        "description": "desc"
+    })
 
-    #reset index and include column header
+    # reset index and include column header
     df_846.reset_index(inplace=True)
     df_846.set_index('index', drop=False, inplace=True)
-    
-    #re-order columns
-    df_846 = df_846[['src', 'state', 'city', 'desc', 'tags', 'geolocation', 'title', 'date', 'date_text', 'id', 'lat', 'long']]
 
-    df_846.to_csv('df_846.csv')
+    # re-order columns
+    df_846 = df_846[
+        ['src', 'state', 'city', 'desc', 'tags', 'geolocation', 'title', 'date',
+         'date_text', 'id', 'lat', 'long']]
 
-
+    return df_846
